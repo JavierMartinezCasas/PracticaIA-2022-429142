7bc49e34aee0998bfc98a03433f8f50c0c8db758
Javier <100429142@alumnos.uc3m.es>
2022-05-06 16:46:02 +0200

    Pruebas csv

--- pruebas_csv.py
+++ pruebas_csv.py
@@ -1,8 +1,15 @@
 import csv
 import pandas as pd
 
-states = ['HHH', 'HHL', 'HLH', 'LHH', 'LLH', 'LHL', 'HLL', 'LLL'] # Los estados posibles (se pueden representar de 0 a 7)
-contadorvHHH
+# Los estados posibles (se pueden representar de 0 a 7)
+states = ['HHH', 'HHL', 'HLH', 'LHH', 'LLH', 'LHL', 'HLL', 'LLL']
+
+# contadorHHH_HHH = 0  Se usaria para contar las veces que HHH pasa a HHH (para probabilidades)
+# contadorHHH_HHL = 0
+
+matrizN = [[]]
+matrizE = [[]]
+matrizW = [[]]
 
 file = open('Data.csv')
 type(file)
@@ -13,57 +20,106 @@ rows = []
 for row in csvreader:
     rows.append(row)
 
-columns = ['Initial traffic level N', 'Initial traffic level E', 'Initial traffic level W', 'Final traffic level N', 'Final traffic level E', 'Final traffic level W']
+columns = ['Initial traffic level N', 'Initial traffic level E', 'Initial traffic level W', 'Green traffic light',
+           'Final traffic level N', 'Final traffic level E', 'Final traffic level W']
 
 df = pd.read_csv('Data.csv', sep=";", usecols=columns)
-print(df)
 
 # Hay que coger toda la fila, contar las veces que aparece, y dividarla entre el número total de filas [len(rows)]
 
-for i in range(len(rows)):
+# for i in range(len(rows)):
 
-
 for i in range(len(rows)):
+    action = df['Green traffic light']
+    _action = action[i]
+
     initialN = df['Initial traffic level N']
     if initialN[i] == 'High':
-        _initialN = 'H'
+        _initialN = 1  # H
     else:
-        _initialN = 'L'
+        _initialN = 0  # L
 
     initialE = df['Initial traffic level E']
     if initialE[i] == 'High':
-        _initialE = 'H'
+        _initialE = 1
     else:
-        _initialE = 'L'
+        _initialE = 0
 
     initialW = df['Initial traffic level W']
     if initialW[i] == 'High':
-        _initialW = 'H'
+        _initialW = 1
     else:
-        _initialW = 'L'
+        _initialW = 0
 
     finalN = df['Final traffic level N']
     if finalN[i] == 'High':
-        _finalN = 'H'
+        _finalN = 1
     else:
-        _finalN = 'L'
+        _finalN = 0
 
     finalE = df['Final traffic level E']
     if finalE[i] == 'High':
-        _finalE = 'H'
+        _finalE = 1
     else:
-        _finalE = 'L'
+        _finalE = 0
 
     finalW = df['Final traffic level W']
     if finalW[i] == 'High':
-        _finalW = 'H'
+        _finalW = 1
     else:
-        _finalW = 'L'
+        _finalW = 0
 
-    initial = _initialN + _initialE + _initialW
-    final = _finalN + _finalE + _finalW
-    print(initial)
-    print(final)
+    initial = str(_initialN) + str(_initialE) + str(_initialW) # Número en binario
+    final = str(_finalN) + str(_finalE) + str(_finalW)
+
+    _initial = int(initial, 2)
+    _final = int(final, 2)
+
+    # print("Row " + str(i) + ":")
+    # print("Initial value: " + initial)
+    # print("Action: " + _action)
+    # print("Final value: " + final)
+    # print("-------------------")
+
+    """
+    Fijar primero la acción, luego el estado incial de los semáforos (contamos el total)
+    y finalmente el estado final (dónde contamos las ocurrencias)
+    """
+    totalLLL = 0
+    totalLLH = 0
+
+    # Obtener los datos para la acción N y añadir para la matrizN las probs. de la misma [columnas[filas]]
+    if action[i] == "N":  # Fijamos la acción
+        if int(initial) == 0:  # Fijamos el estado inicial con LLL (000)
+
+            totalLLL += 1
+            for j in range(len(rows)):  # Recorremos los datos para buscar los que el estado inicial sea LLL (000)
+                if int(final) == 0:
+                    print("Hey")
+
+        if int(initial) == 1:
+            totalLLH += 1
+
+
+
+
+        """
+        if int(initial) == 111:
+            initialHHH.append(i)
+        
+        if int(initial) == 110:
+            initialHHH.append(i)
+        
+        if int(final) == 110:
+            final.append(i)
+        """
+    """
+    elif action[i] == "E":
+        print()
+
+    elif action[i] == "W":
+        print()
+    """
+
 
 file.close()
-
