ca556b17b8c1f8404f095b49f798d65680b2f4a6
Javier <100429142@alumnos.uc3m.es>
2022-05-11 17:44:16 +0200

    obtenerValores(), probando

--- pruebasFinal.py
+++ pruebasFinal.py
@@ -5,6 +5,8 @@ import pandas as pd
 NUM_ESTADOS = 8
 NUM_ACCIONES = 3
 COSTE = 1
+ESTADO_OBJETIVO = 0
+LIMITE_CICLOS = 5000
 
 
 def obtenerProbabilidades():
@@ -23,7 +25,7 @@ def obtenerProbabilidades():
 
     df = pd.read_csv('Data.csv', sep=";", usecols=columns)
 
-    for i in range(8785):   # Parra recorrer todas las filas del fichero de datos
+    for i in range(8785):  # Parra recorrer todas las filas del fichero de datos
         action = df['Green traffic light']
         if action[i] == 'N':  # Comprueba que semáforo está encendiendo
             ac = 0  # Le asigna un valor (del 0 al 2)
@@ -75,7 +77,8 @@ def obtenerProbabilidades():
         eo = int(initial, 2)  # Número en decimal del estado origen
         ed = int(final, 2)  # Número en decimal del estado destino
 
-        ocurrencias[ac][eo][ed] = ocurrencias[ac][eo][ed] + 1  # Actualizar la matriz ocurrencias con las ocurrencias de cada linea
+        ocurrencias[ac][eo][ed] = ocurrencias[ac][eo][
+                                      ed] + 1  # Actualizar la matriz ocurrencias con las ocurrencias de cada linea
 
     for ac in range(NUM_ACCIONES):
         for eo in range(NUM_ESTADOS):
@@ -91,21 +94,60 @@ def obtenerProbabilidades():
     return probabilidad
 
 
+# Esta funbción genera una tupla con los costes correspondientes para cada acción
 def calcularCostes():
-    costes = [0 for ac in range(NUM_ACCIONES)]
-
+    costes = [COSTE for ac in range(NUM_ACCIONES)]
     return costes
 
-def obtenerValoresEsperados(costes):
-    valores = [0 for eo in range(NUM_ESTADOS)]
 
+# Esta función itera sobre las ecuaciones de Bellman un número determinado de veces y devuelve una tabla con los valores
+def obtenerValoresEsperados(costes, probabilidad):
+    valores = [0 for eo in range(NUM_ESTADOS)]
+    verdeN = costes[0]
+    verdeE = costes[1]
+    verdeW = costes[2]
+
+    for ac in range(len(probabilidad)):  # ac es 0, 1 o 2 para cada acción
+        if ac == 0:
+            cost = verdeN
+        elif ac == 1:
+            cost = verdeE
+        elif ac == 2:
+            cost = verdeW
+
+        for eo in range(len(probabilidad[ac])):
+            # print(eo)
+            for ed in range(len(probabilidad[ac][eo])):  # Cada elemento de aquí es un probabilidad[ac][eo][ed]
+                # print(ed)
+                valor = cost + probabilidad[ac][eo][ed] * valores[ed]
+                valores[ed] = valor
+
+    """
+    # Plantear las ecuaciones de Bellman
+    for ac in range(probabilidad):  #Recorremos para los 3 diferentes estados
+        if ac == 0:
+            cost = verdeN   # Asignamos el coste de la tupla costes
+        for eo in range(probabilidad):
+            for ed in range(probabilidad):
+                print(probabilidad[ac][eo][ed])
+
+        if ac == 1:
+            cost = verdeE
+        for eo in range(probabilidad):
+            for ed in range(probabilidad):
+                print(probabilidad[ac][eo][ed])
+
+        if ac == 2:
+            cost = verdeW
+        for eo in range(probabilidad):
+            for ed in range(probabilidad):
+                print(probabilidad[ac][eo][ed])
+    """
     return valores
 
 
 probabilidad = obtenerProbabilidades()  # Obtener las probabilidades para las ecuaciones
 costes = calcularCostes()
-valores = obtenerValoresEsperados(costes)
+valores = obtenerValoresEsperados(costes, probabilidad)
 
-print(probabilidad)
-print(costes)
 print(valores)
